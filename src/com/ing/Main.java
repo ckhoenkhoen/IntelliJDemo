7522ee9eb52cecbed979ff03a87b7f387071f32a
ckhoenkhoen <chander.khoenkhoen@ing.com>
2018-06-13 14:56:26 +0200

    3e commit

--- src/com/ing/Main.java
+++ src/com/ing/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here!!
         System.out.println("Hello, world!");
+
+        // Added a new printline
+        System.out.println("Hello, world 3!");
     }
 }
