fac477c50b12a3ff35e6c7bbe830e552325c50f4
ckhoenkhoen <38432457+ckhoenkhoen@users.noreply.github.com>
2018-06-13 14:55:59 +0200

    Update Main.java

--- src/com/ing/Main.java
+++ src/com/ing/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here!!
-        System.out.println("Hello, world!");
+        System.out.println("Hello, world 1!");
+	System.out.println("Hello, world 2!");
     }
 }
