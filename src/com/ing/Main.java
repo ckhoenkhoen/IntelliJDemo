25e7ddf3524c6940d21f63ad4f14496d5e772430
ckhoenkhoen <38432457+ckhoenkhoen@users.noreply.github.com>
2018-06-13 15:04:49 +0200

    Update Main.java

--- src/com/ing/Main.java
+++ src/com/ing/Main.java
@@ -3,7 +3,7 @@ package com.ing;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here!!
-        System.out.println("Hello, world!");
+	// write your code here!
+        System.out.println("Hallo, wereld!");
     }
 }
